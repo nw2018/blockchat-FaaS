a1d6b3503ee5373fcdfd412f5bac90686f69e601
Lu Jiacheng <Jason0214@users.noreply.github.com>
2018-01-18 00:32:03 -0800

    :bug: woops

--- functions/__main__.js
+++ functions/__main__.js
@@ -5,7 +5,7 @@
 module.exports = (callback) => {
 
   callback(null, 
-    new Buffer(`This is the API for BlockChat, for more information visit <a href="">app.block-chat.com</a>`),
+    new Buffer(`This is the API for BlockChat, for more information visit <a href="://app.block-chat.com">app.block-chat.com</a>`),
     {'Content-Type': 'text/html; charset=utf-8'}
   );
 };
